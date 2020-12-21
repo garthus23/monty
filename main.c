afda7d8781bbf84174f9cacf5124e22972bd369a
Gregory ARTHUS <garthus@adminux.fr>
2020-12-21 09:59:05 +0000

    add(): print a file entered in argument

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include "monty.h"
 
 
-int main(void)
+int main(char** argc, char** argv)
 {
-	
+	char c;
+	FILE *fptr;
 
-
-
-
+	if ((fptr = fopen(argv[1],"r")) != NULL)
+	{
+		c = fgetc(fptr); 
+		while (c != EOF) 
+		{ 
+			printf ("%c", c); 
+			c = fgetc(fptr); 
+		} 
+  
+		fclose(fptr); 
+	}
+	else
+	{
+		printf("On arrive pas a ouvrir ton fichier\n");
+	}
+	return (0);
 }
