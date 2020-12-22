f1765532f5292a4e1000a75f96cbdfd2aafc967c
Gregory ARTHUS <garthus@adminux.fr>
2020-12-22 04:46:58 +0100

    debug(): betty style

--- push_func.c
+++ push_func.c
@@ -1,13 +1,46 @@
 #include "monty.h"
 
+char **arr;
+
+/**
+ * push_int - verify if there is a number to add
+ * Return: 1 when there is one
+**/
+
+int push_int(void)
+{
+	int i;
+
+	if (arr[1])
+	{
+		for (i = 0; arr[1][i] != '\0'; i++)
+		{
+			if (isdigit(arr[1][i]) == 0)
+				return (0);
+		}
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * push_func - add an int to the head
+ * @h: a double linked list
+ * @j: an int
+ * Return: nothing
+**/
 
 void push_func(stack_t **h, unsigned int j)
 {
-	extern char **arr;
+	int i, digit = 1;
 	stack_t *new;
 
+	if (push_int() == 0)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", j);
+		exit(EXIT_FAILURE);
+	}
 	new = malloc(sizeof(stack_t));
-
 	if (new == NULL)
 		exit(0);
 
@@ -23,4 +56,5 @@ void push_func(stack_t **h, unsigned int j)
 	}
 	new->prev = NULL;
 	*h = new;
+
 }
