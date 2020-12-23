4a7078fcda39a06e362f64423a90a419d96f574e
Gregory ARTHUS <garthus@adminux.fr>
2020-12-23 18:19:25 +0100

    debug(): white space at start

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -1,1 +1,6 @@
-push 
+push 3
+    push       2	
+   push  	5	toto
+
+   push 	1 	m
+pall
--- push_int.c
+++ push_int.c
@@ -1,12 +1,29 @@
 #include "monty.h"
 
 /**
- * push_int - verify if there is a number to add
+ * exit_push_int - exit the push int fonction
  * @h: a doubly linked list
  * @j: line of the opened file
  * @fptr: pointer on open file
  * Return: 1 when there is one
 **/
+int exit_push_int(stack_t *h, int j, FILE *fptr)
+{
+	fprintf(stderr, "L%d: usage: push integer\n", j);
+	free_all(arr, h);
+	fclose(fptr);
+	exit(EXIT_FAILURE);
+}
+
+
+
+/**
+ * push_int - verify if there is a number to add
+ * @h: a doubly linked list
+ * @j: line of the opened file
+ * @fptr: pointer on open file
+ * Return: 1 when there is one
+ **/
 int push_int(stack_t *h, int j, FILE *fptr)
 {
 	int i = 0, push = 1;
@@ -23,10 +40,10 @@ int push_int(stack_t *h, int j, FILE *fptr)
 	i = 0;
 	if ((push == 1 && !arr[1]) || (push == 1 && arr[1][0] == '\0'))
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", j);
-		free_all(arr, h);
-		fclose(fptr);
-		exit(EXIT_FAILURE);
+		if (arr[0][0] != '\0')
+		{
+			exit_push_int(h, j, fptr);
+		}
 	}
 
 	i = 0;
@@ -38,10 +55,7 @@ int push_int(stack_t *h, int j, FILE *fptr)
 		{
 			if (isdigit(arr[1][i]) == 0)
 			{
-				fprintf(stderr, "L%d: usage: push integer\n", j);
-				free_all(arr, h);
-				fclose(fptr);
-				exit(EXIT_FAILURE);
+				exit_push_int(h, j, fptr);
 			}
 		}
 		return (1);
