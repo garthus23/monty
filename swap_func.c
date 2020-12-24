4a012d0f019042b4085816e5de4c3a939d79563e
Gregory ARTHUS <garthus@adminux.fr>
2020-12-24 01:13:57 +0100

    debug(): pall function with string

--- swap_func.c
+++ swap_func.c
@@ -14,14 +14,16 @@ void swap_func(stack_t **h, unsigned int j)
 	int tmp;
 
 	if (*h)
+	{
 		current = *h;
 
-	if (current->next != NULL)
-	{
-		current = current->next;
-		tmp = current->n;
-		current->n = current->prev->n;
-		current->prev->n = tmp;
+		if (current->next != NULL)
+		{
+			current = current->next;
+			tmp = current->n;
+			current->n = current->prev->n;
+			current->prev->n = tmp;
+		}
 	}
 	else
 	{
