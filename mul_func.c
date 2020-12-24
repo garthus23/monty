6688a70c33a95ee21d9ba9ff52c9800983c0b543
Gregory ARTHUS <garthus@adminux.fr>
2020-12-24 01:35:00 +0100

    debug(): less than two elements

--- mul_func.c
+++ mul_func.c
@@ -5,22 +5,29 @@
  * @h: a doubly linked list
  * @j: an index in a list
  * Return: Nothing
-**/
+ **/
 
 void mul_func(stack_t **h, unsigned int j)
 {
 	stack_t *current;
 
 	if (*h)
+	{
 		current = *h;
 
-	if (current->next)
-	{
-		current->next->n = current->next->n * current->n;
-		current->next->prev = NULL;
-		*h = current->next;
-		current->next = NULL;
-		free(current);
+		if (current->next)
+		{
+			current->next->n = current->next->n * current->n;
+			current->next->prev = NULL;
+			*h = current->next;
+			current->next = NULL;
+			free(current);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: can't mul, stack too short\n", j);
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
