5bd902fe356b52f6f99fa999cf20f36433039df6
Gregory ARTHUS <garthus@adminux.fr>
2020-12-24 01:23:31 +0100

    debug(): add at first line

--- add_func.c
+++ add_func.c
@@ -12,15 +12,17 @@ void add_func(stack_t **h, unsigned int j)
 	stack_t *current;
 
 	if (*h)
+	{
 		current = *h;
 
-	if (current->next)
-	{
-		current->next->n = current->next->n + current->n;
-		current->next->prev = NULL;
-		*h = current->next;
-		current->next = NULL;
-		free(current);
+		if (current->next)
+		{
+			current->next->n = current->next->n + current->n;
+			current->next->prev = NULL;
+			*h = current->next;
+			current->next = NULL;
+			free(current);
+		}
 	}
 	else
 	{
